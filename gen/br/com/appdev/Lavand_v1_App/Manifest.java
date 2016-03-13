a0eab1ba6240a5481eec12d0849f4f01e935b3a1
Jun Hong S <junx.03@gmail.com>
2016-03-13 20:37:08 -0300

    Initial commit

---
+++ gen/br/com/appdev/Lavand_v1_App/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package br.com.appdev.Lavand_v1_App;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
---
+++ src/br/com/appdev/Lavand_v1_App/MyActivity.java
@@ -0,0 +1,15 @@
+package br.com.appdev.Lavand_v1_App;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
